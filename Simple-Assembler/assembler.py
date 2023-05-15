6ca273951314188fec9c7507aa80519a5dc1408c
Ritika Thakur <ritika22408@iiitd.ac.in>
2023-05-15 21:36:27 +0530

    Type F function

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -732,6 +732,17 @@ def E(i,j):
     f.write(s+"\n")
     f.close()
 
+def F(i,j):
+    f=open("output{}.txt".format(j),"a");
+    unused="00000000000"
+    if (i[0] in list(labels_locations.values())):
+        s=(TYPE_F[i[1]]+unused)
+    else:
+        s=(TYPE_F[i[0]] + unused)
+    # print(s)
+    f.write(s+"\n")
+    f.close()
+
 def main():
     global R0,R1,R2,R3,R4,R5,R6,R7,FLAGS
     global REGISTERS,INSTRUCTIONS,variables_locations,labels_locations,TYPE_A,TYPE_B,TYPE_C,TYPE_D,TYPE_E,TYPE_F,type_A,type_B,type_C
