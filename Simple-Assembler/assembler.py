2a2c2af803f3e9ea53c293058051e3f107f00feb
Muthuraj-Vairamuthu <muthuraj22307@iiitd.ac.in>
2023-05-15 20:56:00 +0530

    added functions

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -9,6 +9,37 @@ def binary(n):
         s="0"+s
     return s
 
+def return_code(name):
+    f1=open("{}".format(name),"r")
+    x=f1.read().split("\n")
+    # print(x)
+    for i in range(len(x)):
+        x[i]=x[i].strip()
+    x_copy=x.copy()
+    dict_of_code={}
+    bin_dict_of_code={}
+    for i in range(len(x_copy)):
+        if(x_copy[i]==""):
+            x.remove("")
+    for i in range(len(x)):
+        x[i]=x[i].split()
+    # print(x)
+    for i in range(len(x)):
+        dict_of_code[i+1]=x[i]
+    # print(dict_of_code)
+    f1.close()
+    binary_instructions={}
+    count=0
+    for i,j in dict_of_code.items():
+        if(j[0]=="var"):
+            count+=1
+    for i,j in dict_of_code.items():
+        if(j[0]=="var"):
+            continue
+        bin_dict_of_code[binary(i-count-1)]=j
+    # print(bin_dict_of_code)
+    return dict_of_code,bin_dict_of_code
+
 def main():
     global R0,R1,R2,R3,R4,R5,R6,R7,FLAGS
     global REGISTERS,INSTRUCTIONS,variables_locations,labels_locations,TYPE_A,TYPE_B,TYPE_C,TYPE_D,TYPE_E,TYPE_F,type_A,type_B,type_C
@@ -36,3 +67,4 @@ def main():
     REGISTERS={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
 
 main()
+
